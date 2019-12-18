e8f02ed36a53193a8e3431ec994ad51857a4fd5f
Wahyu Berlianto <wahyubrr@imajinasi.local>
2019-12-18 12:27:12 +0700

    add comment

--- peer.py
+++ peer.py
@@ -9,6 +9,7 @@ randhigh = 20
 randlow = 10
 counter = 0
 
+# counter function, if reaching 0, be a leader
 def counter():
 	global counter
 	counter = random.randint(randlow, randhigh)
@@ -20,30 +21,38 @@ def counter():
 	client.unsubscribe("topic/leader_heartbeat")
 	leaderthread = threading.Thread(target=leader_heartbeat)
 	leaderthread.start()
+
+# send leader heartbeat to anyone to reset their counter
 def leader_heartbeat():
 	while True:
 		print("reset counter")
 		client.publish("topic/leader_heartbeat", "reset")
 		time.sleep(5)
+
+# if there is a message from broker, run this function
 def on_message(client, userdata, message):
 	global counter
 	if str(message.payload.decode("utf-8")) == "reset":
 		print("counter reset")
 		counter = random.randint(randlow, randhigh)
+
+# function for the client to loop forever
 def client_loop_forever():
 	client.loop_forever() #start the loop
 
+# initialize client for mqtt
 print("creating new instance")
 client = mqtt.Client("P%s" % peerid)
 client.on_message=on_message
 print("connecting to broker")
 client.connect("localhost", 1883, 60)
-
 print("Subscribing to topic","topic/leader_heartbeat")
 client.subscribe("topic/leader_heartbeat")
 
+# start client loop in a thread
 loopthread = threading.Thread(target=client_loop_forever)
 loopthread.start()
 
+# start counter in a thread
 counterthread = threading.Thread(target=counter)
 counterthread.start()
